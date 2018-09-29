0596d535b14a5b58906de583fbaed81acd906431
Dmytro <dmytro.ilhanaiev@gmail.com>
2018-09-29 22:50:30 +0300

    Added indexjs, added .DS_Store to gitignore

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: "airbnb",
+  rules: {
+    "react/jsx-filename-extension": [1, {"extensions": [".js"]}],
+    "no-extra-semi": "error",
+    "semi": ["error", "never"],
+    "eol-last": ["error", "always"],
+    "no-unreachable": "error",
+    "no-unexpected-multiline": "error"
+  }
+}
