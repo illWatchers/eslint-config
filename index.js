4288dfda6bc883988b5649e23929beb0b8b50163
Dmytro <dmytro.ilhanaiev@gmail.com>
2018-10-01 04:26:30 +0300

    Universal rules set, to fit back-end as well

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 module.exports = {
-  extends: "airbnb",
   rules: {
-    "react/jsx-filename-extension": [1, {"extensions": [".js"]}],
     "no-extra-semi": "error",
     "semi": ["error", "never"],
     "eol-last": ["error", "always"],
